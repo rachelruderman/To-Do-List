39ae44f8add61ad0ff52ae7bfdd4d485a2ca3fe7
Rachel <raquel.rudermano@gmail.com>
2017-06-03 19:13:58 -0700

    created express app, initialized sequelize, about to create database

---
+++ app.js
@@ -0,0 +1,22 @@
+//app.js is our Express app
+//goal: watch Stephen's vids to figure out what this all means line-by-line
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+//set up the express app
+const app = express();
+
+//Log requests to the console
+app.use(logger('dev'));
+
+//Parse data from incoming requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//Set up a default catch-all route that sends back a welcome message in json
+app.get('*', (req, res) => res.status(200).send({
+  message: 'Welcome to the beginning of nothingness',
+}));
+
+module.exports = app;
